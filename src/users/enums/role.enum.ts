e2b4f71282c341cdcf1bce49996cde314c0ff7f8
iyed <73111142+Iyed73@users.noreply.github.com>
2025-05-31 07:54:06 +0100

    Add friendship feature

--- src/users/enums/role.enum.ts
+++ src/users/enums/role.enum.ts
@@ -1,4 +1,10 @@
+import { registerEnumType } from '@nestjs/graphql';
+
 export enum Role {
   Regular = 'regular',
   Admin = 'admin',
 }
+
+registerEnumType(Role, {
+  name: 'Role',
+});
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Friendship } from './entities/friendship.entity';
+import { FriendsController } from './friendship.controller';
+import { FriendshipService } from './friendship.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Friendship])],
+  controllers: [FriendsController],
+  providers: [FriendshipService],
 })
 export class UsersModule {}
